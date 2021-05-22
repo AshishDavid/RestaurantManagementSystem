c594eed513fa411aa75c477d33b2c486b9138b8f
Moniksama <74771090+Moniksama@users.noreply.github.com>
2021-05-22 18:09:52 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import MySQLdb.cursors
+from os.path import join, dirname, realpath
 from flask import Flask, redirect, render_template, request, url_for, session
 from flask_mysqldb import MySQL
 import bcrypt
@@ -12,7 +13,9 @@ app.config['MYSQL_DB'] = 'geeklogin'
 
 mysql = MySQL(app)
 
-app.secret_key = 'monica'
+app.secret_key = 'pmir@7est'
+app.config['UPLOAD_FOLDER'] = '/static/Images'
+UPLOADS_PATH = join(dirname(realpath(__file__)), 'static/Images')
 
 
 @app.route('/')
@@ -107,6 +110,7 @@ def login():
         if account:
             if bcrypt.hashpw(password, account['password'].encode('utf-8')) == account['password'].encode('utf-8'):
                 session['name'] = username_or_email
+                session['accountType'] = account['accountType']
                 return redirect(url_for("welcome"))
             else:
                 msg = 'Incorrect password !'
@@ -148,6 +152,169 @@ def itemsdishes():
         cursor.close()
 
     return render_template('Dishes/index.html', data=dishesresult)
+
+
+@app.route('/cart', methods=['GET', 'POST'])
+def cart():
+    if request.method == 'GET':
+        return render_template('Orders/cart.html')
+
+
+@app.route('/manage', methods=['GET', 'POST'])
+def manage():
+    if request.method == 'GET':
+        return render_template('Management/index.html')
+
+
+@app.route('/managecategories', methods=['GET', 'POST'])
+def managecategories():
+    if request.method == 'GET':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM categories WHERE enabled = 1')
+        categoriesresult = cursor.fetchall()
+        cursor.close()
+        return render_template('Management/categories.html', data=categoriesresult)
+
+
+@app.route('/managedishes', methods=['GET', 'POST'])
+def managedishes():
+    if request.method == 'GET':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            'SELECT itd.*,ct.name AS categoryname FROM itemdishes itd JOIN categories ct ON itd.categoryId = ct.id WHERE itd.enabled = 1')
+        dishesresult = cursor.fetchall()
+        cursor.close()
+        return render_template('Management/dishes.html', data=dishesresult)
+
+
+@app.route('/createcategory', methods=['GET', 'POST'])
+def createcategory():
+    if request.method == 'GET':
+        return render_template('Categories/create.html')
+    else:
+        namecat = request.form['namecategory']
+        file1 = request.files['imageCategory']
+        path = join(UPLOADS_PATH, file1.filename)
+        file1.save(path)
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO categories (name, imagePath, enabled) VALUES (%s, %s, %s)",
+                       (namecat, join("static/images", file1.filename), True))
+        mysql.connection.commit()
+
+        return redirect(url_for("manage"))
+
+
+@app.route('/deletecategory', methods=['GET', 'POST'])
+def deletecategory():
+    if request.method == 'GET':
+        idcategory = request.args.get('id')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("UPDATE categories SET enabled = 0 WHERE id = %s", idcategory)
+        mysql.connection.commit()
+        cursor.close()
+
+    return redirect(url_for("managecategories"))
+
+
+@app.route('/editcategory', methods=['GET', 'POST'])
+def editcategory():
+    if request.method == 'GET':
+        idcategory = request.args.get('id')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM categories WHERE id = %s', idcategory)
+        categoriesresult = cursor.fetchall()
+        cursor.close()
+        return render_template('Categories/edit.html', data=categoriesresult)
+
+    else:
+        idcat = request.form['idcategory']
+        namecat = request.form['namecategory']
+        imagepath = ''
+        if request.files['imageCategory'].filename != '':
+            file1 = request.files['imageCategory']
+            path = join(UPLOADS_PATH, file1.filename)
+            file1.save(path)
+            imagepath = file1.filename
+        else:
+            imagepath = request.form['imagepath']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("UPDATE categories SET name = %s, imagePath = %s WHERE id = %s", (namecat, imagepath, idcat))
+        mysql.connection.commit()
+        cursor.close()
+
+        return redirect(url_for("managecategories"))
+
+
+@app.route('/createdishes', methods=['GET', 'POST'])
+def createdishes():
+    if request.method == 'GET':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM categories WHERE enabled = 1')
+        categoriesresult = cursor.fetchall()
+        cursor.close()
+        return render_template('Dishes/create.html', data=categoriesresult)
+    else:
+        namedish = request.form['namedish']
+        pricedishes = request.form['pricedishes']
+        descriptiondishes = request.form['descriptiondishes']
+        categoryselect = str(request.form.get('categorySelect'))
+        filedish = request.files['imagedishes']
+        pathdishes = join(UPLOADS_PATH, filedish.filename)
+        filedish.save(pathdishes)
+
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            "INSERT INTO itemdishes (name, description, price, categoryId, imagePath, enabled) VALUES (%s, %s, %s, %s, %s, %s)",
+            (namedish, descriptiondishes, pricedishes, categoryselect, join("static/images", filedish.filename), True))
+        mysql.connection.commit()
+
+        return redirect(url_for("managedishes"))
+
+
+@app.route('/deletedishes', methods=['GET', 'POST'])
+def deletedishes():
+    if request.method == 'GET':
+        iditemdishes = request.args.get('id')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("UPDATE itemdishes SET enabled = 0 WHERE id = %s", iditemdishes)
+        mysql.connection.commit()
+        cursor.close()
+
+    return redirect(url_for("managedishes"))
+
+
+@app.route('/editdishes', methods=['GET', 'POST'])
+def editdishes():
+    if request.method == 'GET':
+        idcategory = request.args.get('id')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM itemdishes WHERE id = %s', idcategory)
+        dishresult = cursor.fetchall()
+        cursor.close()
+        return render_template('Dishes/edit.html', data=dishresult)
+
+    else:
+        id = request.form['iddishes']
+        namedish = request.form['namedish']
+        pricedishes = request.form['pricedishes']
+        descriptiondishes = request.form['descriptiondishes']
+        imagepath = ''
+        if request.files['imagedishes'].filename != '':
+            file1 = request.files['imagedishes']
+            path = join(UPLOADS_PATH, file1.filename)
+            file1.save(path)
+            imagepath = file1.filename
+        else:
+            imagepath = request.form['imagepath']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("UPDATE itemdishes SET name = %s, description = %s, price = %s, imagePath = %s WHERE id = %s", (namedish, descriptiondishes, pricedishes, imagepath, id))
+        mysql.connection.commit()
+        cursor.close()
+
+        return redirect(url_for("managedishes"))
 
 
 if __name__ == '__main__':
--- static/JS/cart/cart.js
+++ static/JS/cart/cart.js
@@ -1,20 +1,147 @@
 $(document).ready(function () {
+    $("#TotalItem").text("0.00 €");
     $('#quatityitem').on('change keyup', function () {
-        var cantidad = $('#quatityitem').val();
-        var precio = $('#priceItem').val();
-        var total = cantidad * precio;
+        var quantity = $('#quatityitem').val();
+        var price = $('#priceItem').val();
+        var total = quantity * price;
         $("#TotalItem").text(total + " €");
     });
 
+    var retrievedObject = localStorage.getItem('cartObject');
+    console.log('retrievedObject: ', JSON.parse(retrievedObject));
+    var cartObject = JSON.parse(retrievedObject);
+    if (cartObject) {
+        $('table#tableCart tr#defaultrow').remove();
+        //add elements of arreglo
+        var totalToPay = 0.00;
+        for (var i = 0; i < cartObject.length; i++) {
+            totalToPay = totalToPay + parseFloat((cartObject[i].price * cartObject[i].quantity));
+        }
+        $('#valueTot').text(totalToPay + " €");
+
+        for (var i = 0; i < cartObject.length; i++) {
+            var table = document.getElementById("tableCart");
+            if (table) {
+                var row = table.insertRow(-1); // -1 indica que los rows o filas se iran agregando al final de la tabla
+                var ColName = row.insertCell(0); // posicion de columna 0 -> primera 1 -> segunda
+                var ColDescription = row.insertCell(1);
+                var ColQuantity = row.insertCell(2); // posicion de columna 0 -> primera 1 -> segunda
+                var ColPrice = row.insertCell(3);
+                var ColTotal = row.insertCell(4);
+                var ColImage = row.insertCell(5);
+                var ColAction = row.insertCell(6);
+                ColName.innerHTML = cartObject[i].name; //seteando los valores a los nuevos rows
+                ColDescription.innerHTML = cartObject[i].description;
+                ColQuantity.innerHTML = cartObject[i].quantity;
+                ColPrice.innerHTML = cartObject[i].price + " €";
+                ColTotal.innerHTML = cartObject[i].total + " €";
+                ColImage.innerHTML = '<img src=' + cartObject[i].pathImage + ' style="height:200px;width:200px">';
+                ColAction.innerHTML = '<span style="height: 34px; width: 34px;" class="btn-ripple animate"></span> <i onclick="DeleteLine(this)" class="fa fa-trash" data-toggle="tooltip" title="" data-original-title="Delete item"></i>';
+            }
+        }
+    }else{
+        $('#valueTot').text(" 0.00€");
+    }
 });
 
+let arrayObjectCart = [], objectCart = {};
+
+function AddCartItem() {
+    var retrievedObject = localStorage.getItem('cartObject');
+
+    var quantity = $('#quatityitem').val();
+    var price = $('#priceItem').val();
+    var total = quantity * price;
+    var name = $("#cartName").text();
+    var pathImg = $('#pathImage').val();
+    var id = $('#idItem').val();
+    var description = $('#descriptionItem').val();
+    objectCart = {
+        id: id,
+        description: description,
+        quantity: quantity,
+        price: price,
+        total: total,
+        name: name,
+        pathImage: pathImg
+    }
+    if (retrievedObject) {
+        arrayObjectCart = JSON.parse(retrievedObject);
+        arrayObjectCart.push(objectCart);
+        localStorage.setItem('cartObject', JSON.stringify(arrayObjectCart));
+    } else {
+        arrayObjectCart.push(objectCart);
+        localStorage.setItem('cartObject', JSON.stringify(arrayObjectCart));
+    }
+    //add elements of array
+    var totalToPay = 0.00;
+    for (var i = 0; i < arrayObjectCart.length; i++) {
+        totalToPay = totalToPay + parseFloat((arrayObjectCart[i].price * arrayObjectCart[i].quantity));
+    }
+    $('#valueTot').text(totalToPay + " €");
+    $('#CartModalItem').modal('hide');
+}
 
 
-
-function AddCartItem(id, price, name, pathImage) {
+function OpenPreviewCart(id, price, name, pathImage, description) {
     $("#CartModalItem").modal("toggle");
     $('#priceItem').val(price);
     $("#priceItem").text(price);
     $("#cartName").text(name);
+    $("#previewToCart").attr("src", pathImage);
+    $('#pathImage').val(pathImage);
+    $('#idItem').val(id);
+    $('#descriptionItem').val(description);
+
 }
 
+
+function ClearCartAction() {
+    window.localStorage.clear();
+    location.reload();
+}
+
+function DeleteLine(element) {
+    var retrievedObject = localStorage.getItem('cartObject');
+    var Row = element.parentNode.parentNode.rowIndex; //method to get object to delete
+    var name = "";
+    var quantity = "";
+    var index = 0;
+    $('table tr:eq("' + Row + '") td').each(function () {
+        if (index === 0) { // columna 1
+            name = $(this).text();
+        } else if (index === 2) { //columna 2
+            quantity = $(this).text();
+        }
+        index++;
+    });
+
+    arrayObjectCart = JSON.parse(retrievedObject);
+
+    //delete object in array
+    arrayObjectCart.splice(arrayObjectCart.findIndex(matchesEl), 1);
+
+    function matchesEl(el) { //search element in array
+        return (el.name === name && el.quantity === quantity);
+    }
+
+    document.getElementById("tableCart").deleteRow(Row); // eliminacion de elemento de la tabla
+
+    //knowing total
+    //sum elements of array
+    var totalToPay = 0.00;
+    for (var i = 0; i < arrayObjectCart.length; i++) {
+        totalToPay = totalToPay + parseFloat((arrayObjectCart[i].price * arrayObjectCart[i].quantity));
+    }
+    $('#valueTot').text(totalToPay + " €");
+
+    //update  local storage
+    localStorage.setItem('cartObject', JSON.stringify(arrayObjectCart));
+
+
+    var TablaLineas = document.getElementById("tableCart");
+    var rowCount = TablaLineas.rows.length;
+    if (rowCount === 1) {
+        $('#tableCart').append('<tr id="defaultrow"><td colspan="3"><span style="padding-left: 30%;">No items added to cart</span></td></tr>');
+    }
+}
